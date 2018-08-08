e3d0f78a97f547aaf3ff6c32a5d1fb91cef34a5c
Jawahar Chigurupati <cjawahar@gmail.com>
2018-08-08 16:34:15 -0700

    Projects page additions

--- server.js
+++ server.js
@@ -44,6 +44,14 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcome: 'Welcome to Jawahar\'s Projects page!'
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
